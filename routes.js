5ff183f4a522fdb330cc14470e4301bbed8ca943
Nick Doiron <ndoiron@mapmeld.com>
2013-06-26 09:38:07 -0400

    request Boston permits and output to file

--- routes.js
+++ routes.js
@@ -1,6 +1,8 @@
 module.exports = function(app){
 
   var request = require('request');
+  var libxml = require('libxmljs');
+  var fs = require('fs');
 
   app.get('/', function(req, res){
     res.render('map', { });
@@ -13,6 +15,52 @@ module.exports = function(app){
     request(requestOptions, function (err, response, b) {
       b = b.split(',');
       res.json({ x: b[1] * 1.0, y: b[0] * 1.0 });
+    });
+  });
+  
+  app.get('/update_from_gis', function(req, res){
+    var requestOptions = {
+      'uri': 'http://maps.cityofboston.gov/ArcGIS/rest/services/Permitting/Permits/MapServer/0/query?text=&geometry=&geometryType=esriGeometryPoint&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&objectIds=&where=1%3D1&time=&returnCountOnly=false&returnIdsOnly=false&returnGeometry=true&maxAllowableOffset=&outSR=4326&outFields=*&f=pjson'
+    };
+    request(requestOptions, function (err, response, b) {
+      var esriJSON = JSON.parse( b );
+      var points = esriJSON.features;
+      var osmdoc = new libxml.Document().node('osm').attr({
+        version: "0.6",
+        generator: "geogit-viz"
+      });
+      for(var pt=0;pt<points.length;pt++){
+        var permit = osmdoc.node('node').attr({
+          id: points[pt].attributes.APBLDGKEY, // I think this is a unique key
+          lat: points[pt].geometry.y,
+          lng: points[pt].geometry.x,
+          user: 'xx-0',
+          uid: 'xx-0',
+          visible: 'true',
+          version: '1',
+          changeset: '1',
+          timestamp: '2008-09-21T00:00:00Z'
+        });
+        for(key in points[pt].attributes){
+          if(key == "APBLDGKEY" || key == "GPSY" || key == "GPSX"){
+            continue;
+          }
+          permit.node('tag').attr({
+            k: key,
+            v: points[pt].attributes[key]
+          });
+        }
+      }
+      //res.setHeader('Content-Type', 'text/xml');
+      //res.send( osmdoc.toString() );
+      fs.writeFile('osmout.osm', osmdoc.toString(), function(err){
+        if(err){
+          res.json( err );
+        }
+        else{
+          res.send('File done!');
+        }
+      });
     });
   });
   
