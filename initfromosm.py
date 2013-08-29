f1636102f8f38d8c9cca6e31ceb73897b4732c1a
Nick Doiron <ndoiron@mapmeld.com>
2013-08-29 17:35:06 +0000

    patch initfromosm

--- initfromosm.py
+++ initfromosm.py
@@ -3,6 +3,6 @@ import sys, os
 coords = sys.argv
 
 os.system('geogit init')
-os.system('geogit osm download --bbox ' + ' '.join( coords ))
+os.system('geogit osm download --bbox "' + sys.argv[1] + '" "' + sys.argv[2] + '" "' + sys.argv[3] + '" "' + sys.argv[4] + '"')
 os.system('geogit add')
 os.system('geogit commit -m "initial commit"')
